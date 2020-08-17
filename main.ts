7cda94bc897af2060d08533cc9298db8cfe35fa6
Bob Reselman <reselbob@gmail.com>
2020-08-17 14:17:50 -0700

    updated main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
 const wasmCode = await Deno.readFile("./rust_for_deno/target/wasm32-unknown-unknown/debug/rust_for_deno.wasm");
 const wasmModule = new WebAssembly.Module(wasmCode);
 const wasmInstance = new WebAssembly.Instance(wasmModule);
-const {
-    square,
-} = wasmInstance.exports;
+const {square} = wasmInstance.exports;
+
+console.log(square(1));
+console.log(square(2));
+console.log(square(3));
+console.log(square(4));
